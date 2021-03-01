89152b2409405797e7df61d1f4d87a79bf3bdb30
haninder <haninder@crossml.com>
2021-03-01 17:58:58 +0530

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color:"#fff"}}>Hello Sir</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ web-build/precache-manifest.93535a11f668cf69b9f8c1a754fbb105.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "be6b4e97083312375247",
+    "url": "/static/js/app.3aec7469.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "8ffeb20211e52ebb4ab8",
+    "url": "/static/js/2.c3c0c129.chunk.js"
+  },
+  {
+    "revision": "780510f4359c2e7f97887703bd2248ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3c0c129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "6776a1e1974f3b6a834ddf261737ba92",
+    "url": "/manifest.json"
+  }
+];
