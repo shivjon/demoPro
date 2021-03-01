af455d138a38c3458548e80d1e8d18af5c35f1d0
haninder <haninder@crossml.com>
2021-03-01 19:03:16 +0530

    1

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ImageBackground source={{
+        uri:'https://image.freepik.com/free-vector/red-blue-element-design-abstract-modern-background-with-hexagon-diagonal_64718-427.jpg'
+      }} 
+       style={{width:'100%',height:'100%', justifyContent:'center',alignItems:'center'}} 
+       resizeMode="contain"
+       >
       <Text style={{color:"#fff"}}>Hello Sir</Text>
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -15,7 +22,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
